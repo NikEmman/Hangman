87e0a110f12f76981ad75c81ca69658820a428b7
NikEmman <121699424+NikEmman@users.noreply.github.com>
2023-12-10 20:48:26 +0200

    Added user input request and validation methods

--- game.rb
+++ game.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
+require_relative 'welcome'
 class Game
+    attr_accessor :secret_word , :guesses
     def initialize
         @words_array =[]
+        @guess
     end
 
     def get_words
@@ -10,7 +13,37 @@ class Game
 
     def choose_word
         get_words
-        @words_array[rand(0...@words_array.size-1)]
+        secret_word = @words_array[rand(0...@words_array.size-1)]
     end
 
+    def get_user_guess
+       Welcome.new.request_guess
+       @guess = gets.chomp.upcase
+       
+    end
+
+    def validate
+      if @guess.size == 1
+        true
+      else
+        Welcome.new.wrong_input
+        false
+      end
+    end
+    
+    def validated_guess
+      input = ""
+      loop do
+         input = get_user_guess
+         break if validate
+      end
+      @guess = input 
+    end
+
+    def update_guesses
+      guesses.push(@guess)
+    end
 end
+
+a = Game.new.validated_guess
+
--- welcome.rb
+++ welcome.rb
@@ -4,7 +4,8 @@ class Welcome
 To start a new game type [new], to load a saved game type [load], to learn about the game rules type [help] 
 } # W_I_P
 HELP_TEXT = %{ } # W_I_P 
-
+WRONG_INPUT = "Wrong input, type a single letter of the English alphabet"
+REQUEST_GUESS = "Type a letter to enter your guess:"
 def welcome
  puts WELCOME_TEXT
 end
@@ -12,5 +13,11 @@ end
 def help
    puts HELP_TEXT
 end
+ def wrong_input
+    puts WRONG_INPUT
+ end
 
+ def request_guess
+    puts REQUEST_GUESS
+ end
 end
