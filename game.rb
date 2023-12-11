98f381a7c3199f7caa73d83b6800491253d4ee61
NikEmman <121699424+NikEmman@users.noreply.github.com>
2023-12-11 13:42:55 +0200

    Added color class and refactored validate methods

--- game.rb
+++ game.rb
@@ -1,91 +1,87 @@
 # frozen_string_literal: true
-require_relative "speech"
+
+require_relative 'speech'
+# this is my game
 class Game
-    attr_accessor :secret_word , :guesses, :round, :hashed_word
-    def initialize
-        @words_array =[]
-        @guess
-    end
+  attr_accessor :secret_word, :guesses, :round, :hashed_word
 
-    def get_words
-        document = File.foreach("dictionary.txt"){|line| @words_array.push(line)  if (line.size>5 && line.size<12)}
-    end
+  def initialize
+    @words_array = []
+  end
 
-    def choose_word
-        get_words
-        secret_word = @words_array[rand(0...@words_array.size-1)]
-    end
+  def get_words
+    File.foreach('dictionary.txt') { |line| @words_array.push(line) if line.size > 5 && line.size < 12 }
+  end
 
-    def hash_secret_word
-      hashed_word = "_" * secret_word.size
-    end
+  def choose_word
+    get_words
+    @secret_word = @words_array[rand(0...@words_array.size - 1)]
+    hash_secret_word
+  end
 
-    def check_secret_word
-        for n in secret_word.size-1
-          if secret_word.split[n] == @guess
-            hashed_word.split[n] = secret_word.split[n]
-          else
-            Speech.new.wrong_guess
-          end
-        end  
-    end
+  def hash_secret_word
+    @hashed_word = '_' * secret_word.size
+  end
 
-    def get_user_guess
-       Speech.new.request_guess
-       @guess = gets.chomp.upcase
-       
-    end
-
-    def valid_input?
-      if ('A'..'Z').to_a.include?(@guess)
-        true
+  def check_secret_word
+    for n in secret_word.size - 1
+      if secret_word.split[n] == @guess
+        hashed_word.split[n] = secret_word.split[n]
       else
-        Speech.new.wrong_input
-        false
+        Speech.new.wrong_guess
       end
     end
-    
-    def validated_guess
-      input = ""
-      loop do
-         input = get_user_guess
-         break if valid_input?
-      end
-      @guess = input 
-    end
+  end
 
-    def update_guesses
-      guesses.push(@guess)
-    end
+  def get_user_guess
+    Speech.new.request_guess
+    @guess = gets.chomp.upcase
+  end
 
-    def update_round
-      round = guesses.size
-    end
-    
-    def end?(hashed_word, secret_word, round)
-      hashed_word == secret_word || round == 10
-    end
+  def valid_input?(guess)
+    ('A'..'Z').to_a.include?(guess)
+  end
 
-    def start 
-      loop
-      if secret_word.nil?
-        Speech.new.welcome
-        choose_word
-      else
-        next
-      end
-      validated_guess
-      update_guesses
-      check_secret_word
-      update_round
-      break if end?
-      #check for restart
-      #restart (by Game.new)
+  def validated_guess
+    loop do
+      get_user_guess
+      break if valid_input?(@guess)
+
+      Speech.new.wrong_input
     end
-      # check guess vs secret word
-      # display board with hanged man and guesses[]
-    end
+  end
+
+  def update_guesses
+    guesses.push(@guess)
+  end
+
+  def update_round
+    @round = guesses.size
+  end
+
+  def end?(hashed_word, secret_word, round)
+    hashed_word == secret_word || round == 10
+  end
+
+  def start
+    # get_word where?
+    loop
+    next unless secret_word.nil?
+
+    Speech.new.welcome
+    choose_word
+
+    validated_guess
+    update_guesses
+    check_secret_word
+    update_round
+    break if end?
+    # check for restart
+    # restart (by Game.new)
+  end
+  # check guess vs secret word
+  # display board with hanged man and guesses[]
 end
 
 a = Game.new
-
+a.start
