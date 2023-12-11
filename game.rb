04a123e866b2e7eaf2bd9abc2fdabd7fcd635060
NikEmman <121699424+NikEmman@users.noreply.github.com>
2023-12-11 20:40:45 +0200

    Added restart and load/help methods

--- game.rb
+++ game.rb
@@ -1,50 +1,77 @@
+# rubocop:disable Style/StringLiterals
 # frozen_string_literal: true
 
-require_relative 'speech'
-# this is my game
+require_relative "speech"
+# this is my game controller
 class Game
   attr_accessor :secret_word, :guesses, :round, :hashed_word
 
   def initialize
     @words_array = []
+    @welcome_input = nil
   end
 
-  def get_words
-    File.foreach('dictionary.txt') { |line| @words_array.push(line) if line.size > 5 && line.size < 12 }
+  def words_pool
+    File.foreach("dictionary.txt") { |line| @words_array.push(line) if line.size > 5 && line.size < 12 }
   end
 
   def choose_word
-    get_words
+    words_pool
     @secret_word = @words_array[rand(0...@words_array.size - 1)]
     hash_secret_word
   end
 
-  def hash_secret_word
-    @hashed_word = '_' * secret_word.size
-  end
-
-  def check_secret_word
-    for n in secret_word.size - 1
-      if secret_word.split[n] == @guess
-        hashed_word.split[n] = secret_word.split[n]
-      else
-        Speech.new.wrong_guess
-      end
+  def save_load_help
+    case @welcome_input
+    when "help"
+    # display_help method
+    when "load"
+    # load_game method
+    when "new"
+      next
+    else
+      Speech.new.too_hard
     end
   end
 
-  def get_user_guess
+  def display_help
+    Speech.new.help
+    gets
+  end
+
+  def hash_secret_word
+    @hashed_word = "_" * secret_word.size
+  end
+
+  def check_secret_word
+    hashed_word = hashed_word.chars
+    secret_word.chars.each_with_index do |char, index|
+      if char == @guess
+        hashed_word[index] = char
+      else
+        Speech.new.wrong_guess(@guess)
+      end
+    end
+    hashed_word.join
+  end
+
+  def welcome
+    Speech.new.welcome
+    @welcome_input = gets.chomp
+  end
+
+  def request_guess
     Speech.new.request_guess
     @guess = gets.chomp.upcase
   end
 
   def valid_input?(guess)
-    ('A'..'Z').to_a.include?(guess)
+    ("A".."Z").to_a.include?(guess)
   end
 
   def validated_guess
     loop do
-      get_user_guess
+      request_guess
       break if valid_input?(@guess)
 
       Speech.new.wrong_input
@@ -63,21 +90,27 @@ class Game
     hashed_word == secret_word || round == 10
   end
 
+  def reset?
+    Speech.new.reset
+    a = gets.chomp
+    true if a.upcase == "Y"
+  end
+
   def start
     # get_word where?
-    loop
-    next unless secret_word.nil?
+    loop do
+      next unless secret_word.nil?
 
-    Speech.new.welcome
-    choose_word
-
-    validated_guess
-    update_guesses
-    check_secret_word
-    update_round
-    break if end?
-    # check for restart
-    # restart (by Game.new)
+      Speech.new.welcome
+      choose_word
+      validated_guess
+      update_guesses
+      check_secret_word
+      update_round
+      break if end?
+      # check for restart
+      # restart (by Game.new)
+    end
   end
   # check guess vs secret word
   # display board with hanged man and guesses[]
@@ -85,3 +118,4 @@ end
 
 a = Game.new
 a.start
+# rubocop:enable Style/StringLiterals
--- speech.rb
+++ speech.rb
@@ -1,29 +1,44 @@
+# rubocop:disable Style/StringLiterals
 # frozen_string_literal: true
+
 require_relative "game"
+# text class
 class Speech
-    WELCOME_TEXT = %{------------------WELCOME TO HANGMAN-----------------
-To start a new game type [new], to load a saved game type [load], to learn about the game rules type [help] 
-} # W_I_P
-HELP_TEXT = %{ } # W_I_P 
-WRONG_INPUT = "Wrong input, type a single letter of the English alphabet"
-REQUEST_GUESS = "Type a letter to enter your guess:"
-WRONG_GUESS = "The letter #{Game.guess} does not exit in the Secret Word !!"
-def welcome
- puts WELCOME_TEXT
-end
+  WELCOME_TEXT = %(------------------WELCOME TO HANGMAN-----------------
+To start a new game type [new], to load a saved game type [load], to learn about the game rules type [help]
+) # W_I_P
+  HELP_TEXT = %( This is help text, i hope it helped, press [ENTER] to continue) # W_I_P
+  WRONG_INPUT = "Wrong input, type a single letter of the English alphabet"
+  REQUEST_GUESS = "Type a letter to enter your guess:"
+  WRONG_GUESS = "The letter #{char} does not exit in the Secret Word !!"
+  TOO_HARD = "It was too hard, huh? Ok, try again"
+  RESET = "If you want to play again, type [Y]"
+  def welcome
+    puts WELCOME_TEXT
+  end
 
-def help
-   puts HELP_TEXT
-end
- def wrong_input
+  def help
+    puts HELP_TEXT
+  end
+
+  def wrong_input
     puts WRONG_INPUT
- end
+  end
 
- def request_guess
+  def request_guess
     puts REQUEST_GUESS
- end
+  end
 
- def wrong_guess
-   puts WRONG_GUESS
- end
+  def wrong_guess(_char)
+    puts WRONG_GUESS
+  end
+
+  def too_hard
+    puts TOO_HARD
+  end
+
+  def reset
+    puts RESET
+  end
 end
+# rubocop:enable Style/StringLiterals
